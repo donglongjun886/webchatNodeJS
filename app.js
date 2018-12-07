736f3138e7d63596a5e4e01db04fa1ae359d4268
longlongagocsu <donglongjun886@qq.com>
2018-12-08 00:25:01 +0800

    完成access_token的获取存储及校验

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ var crypto = require('crypto');
 
 var connection = mysql.createConnection({
     host : '47.110.90.203',
+    database : 'wechat',
     user : 'root',
     password : 'Kimi-886',
     port : '3306'
+});
+
+connection.on('error', function(err) {
+    console.log("[mysql error]",err);
 });
 
 var config = {
@@ -19,77 +24,91 @@ var config = {
     checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
 };
 
-const app = express()
+const app = express();
+
 var access_token = null;
 app.get('/wechat', (req,res)=>{
+    console.log('start query');
     var cryptor = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.appid);
-    // 加密模式
+    //tokenValidate();
+    var sql = 'SELECT * FROM token limit 1';
+    connection.query(sql,null,function(err,result){
+        if (err){
+            res.send(err);
+        }
+        if (result == '' || result == undefined || result == null || result.size == 0 || expireValidate(result[0])){
+            var url = 'https://api.weixin.qq.com/cgi-bin/token';
+            axios.get( url , {
+                params:{
+                    grant_type:'client_credential',
+                    appid:config.appid,
+                    secret:config.secret
+                }
+            }).then((userinfo)=>{
+                var udata = userinfo.data;
+                var sql = 'INSERT INTO token(access_token, expires_in, create_at) VALUES(?, ?, ?)';
+                var fields = [udata.access_token,udata.expires_in,Date.now()];
+                connection.query(sql, fields, function (err, result) {
+                    if (err){
+                        console.log('[INSERT ERRO]-'+err);
+                    }
+                    return;
+                });
+                access_token = udata.access_token;
+                main(req,res);
+            });
+        }else{
+            console.log('mysql query success');
+            access_token = result[0].access_token;
+            main(req,res);
+        }
+    })
+});
+
+var main = function(req,res){
+    console.info('main function access_token='+access_token);
+    res.send('success');
+}
+
+var tokenValidate = function(){
     var signature = req.query.signature;
     var timestamp = req.query.timestamp;
     var nonce = req.query.nonce;
     var echostr = req.query.echostr;
-    var str = [config.token, timestamp, nonce].sort().join('')
+    var str = [config.token, timestamp, nonce].sort().join('');
     var sha = sha1(str)
     if (sha === signature) {
         res.send(echostr + '');
     } else {
         res.send('');
     }
-});
+}
+
+var expireValidate = function(tokenData){
+    if (tokenData == undefined || tokenData == null){
+        return true;
+    }
+    var create_at = tokenData.create_at;
+    var expires_in = tokenData.expires_in;
+    var expireTime = create_at+expires_in*1000;
+    var now = Date.now();
+    if (now<expireTime){
+        connection.query('delete from token',null,function (err,result) {
+            if (err){
+                console.log('[DELETE ERRO]-'+err);
+            }
+            return;
+        });
+        return true
+    }else{
+        return false;
+    }
+}
 
 var sha1 = function (str){
     var shasum = crypto.createHash('sha1');
     return shasum.update(str).digest('hex');
 }
 
-var main = function(){
-    console.info('main function')
-}
 
-
-var queryToken = function(config){
-    console.log('start queryToken');
-    var tokenUrl = 'https://api.weixin.qq.com/cgi-bin/token';
-    axios.get(tokenUrl,{
-        params:{
-            grant_type:'client_credential',
-            appid:config.appid,
-            secret:config.secret
-        }
-    }).then((userinfo)=>{
-        console.log('userinfo.data.access_token:'+userinfo.data.access_token);
-        var access_token = userinfo.data.access_token ;
-        saveToken(userinfo.data);
-    });
-}
-
-var getToken = function(config){
-    console.log('start getToken');
-    var sql = 'SELECT * FROM token WHERE openid = ?';
-    connection.query(sql, null, function (err, result) {
-        if(err) {
-            return console.error('err:'+err);
-        }
-        if (result==null || result.size==0){
-            queryToken(config)
-        }
-        access_token = result[0];
-    });
-}
-
-var saveToken = function(token){
-    console.log('start saveToken');
-    var sql = 'REPLACE INTO token(access_token, expires_in, create_at) VALUES(?, ?, ?)';
-    var fields = [token.access_token, token.expires_in, Date.now()];
-    connection.query(sql, fields, function (err, result) {
-        if (err){
-            return console.error(err);
-        }
-        access_token = result[0];
-        main();
-    });
-}
-
-
-
-app.listen(80, () => console.log('Example app listening on port 80!'));
+app.listen(3000, () => console.log('Example app listening on port 80!'));
