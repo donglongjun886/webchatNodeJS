6cae8b270377d31b352d311c15ae95b056d75158
longlongagocsu <donglongjun886@qq.com>
2019-01-23 10:33:16 +0800

    @update:捕获异常，防止异常退出

--- service/app.js
+++ service/app.js
@@ -3,6 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
+
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
+});
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +16,11 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use('/wechat', wechatApi);
+
+app.use(function(err, req, res, next) {
+  console.warn('错误处理中间捕获Exception', err);
+  res.send('内部错误');
+});
 
 app.listen(3000);
 console.log('success listen at port: 3000')
